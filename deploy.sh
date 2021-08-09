aa4f7d521aa0aa147ad84911c30e39055e158fb0
deenadayaalan thirugnanasambandam <deeandat@amazon.com>
2021-08-09 22:19:17 +1000

    sample enterprise controls updated

--- deploy.sh
+++ deploy.sh
@@ -10,32 +10,32 @@ echo Deploying
 npx cdk deploy --require-approval=never
 
 BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name AuditManagerBlogStack --output json | jq -r '.Stacks[].Outputs[] | select ( .OutputKey | contains("bucket") ) | .OutputValue')
-echo Upload Example Controls to S3 bucket $BUCKET_NAME/controls
+echo Upload Example Data Security Controls to S3 bucket $BUCKET_NAME/controls
 
 # upload the controls
-aws s3 cp ./examples/controls/datasecruity-encryption-at-rest.yaml s3://$BUCKET_NAME/controls/
-aws s3 cp ./examples/controls/datasecruity-encryption-in-transit.yaml s3://$BUCKET_NAME/controls/
+aws s3 cp ./examples/controls/datasecurity-encryption-at-rest.yaml s3://$BUCKET_NAME/controls/
+aws s3 cp ./examples/controls/datasecurity-encryption-in-transit.yaml s3://$BUCKET_NAME/controls/
 
 # wait for controls to be created
-echo Waiting for Example Controls to be Created
+echo Waiting for Example Data Security Controls to be Created
 NUM_CONTROLS=0
 while [ $NUM_CONTROLS -ne 2 ]; do
-    NUM_CONTROLS=$(aws auditmanager list-controls --control-type Custom --output json | jq -r '.controlMetadataList[] | select ( .name | test("^Example Control [0-9]?$")) | .name' | wc -l)
+    NUM_CONTROLS=$(aws auditmanager list-controls --control-type Custom --output json | jq -r '.controlMetadataList[] | select ( .name | test("^DataSecurity")) | .name' | wc -l)
     sleep 2
 done
-echo Example Controls are Created
+echo Example Data Security Controls are Created
 
 # upload the frameworks
-echo Upload Example Frameworks to S3 bucket $BUCKET_NAME/frameworks
+echo Upload Example Enterprise Framework to S3 bucket $BUCKET_NAME/frameworks
 aws s3 cp ./examples/frameworks/enterprise-framework.yaml s3://$BUCKET_NAME/frameworks/
 
 # wait for controls to be created
-echo Waiting for Example Frameworks to be Created
+echo Waiting for Example NIST Enterprise Framework to be Created
 NUM_FRAMEWORKS=0
-while [ $NUM_FRAMEWORKS -ne 2 ]; do
-    NUM_FRAMEWORKS=$(aws auditmanager list-assessment-frameworks --framework-type Custom --output json | jq -r '.frameworkMetadataList[] | select ( .name | test("^Example Framework [0-9]?$")) | .name' | wc -l)
+while [ $NUM_FRAMEWORKS -ne 1 ]; do
+    NUM_FRAMEWORKS=$(aws auditmanager list-assessment-frameworks --framework-type Custom --output json | jq -r '.frameworkMetadataList[] | select ( .name | test("^NIST Enterprise")) | .name' | wc -l)
     sleep 2
 done
-echo Example Enterprise Frameworks is Created.
+echo Example NIST Enterprise Frameworks is Created.
 
 echo Deployment Completed
--- lambda/src/controls.ts
+++ lambda/src/controls.ts
@@ -23,92 +23,77 @@ import { updateTags } from './tags';
 const auditManager = new AWS.AuditManager({ apiVersion: '2017-07-25' });
 
 export async function loadCustomControlList(): Promise<AWS.AuditManager.ControlMetadataList> {
-  const results: AWS.AuditManager.ControlMetadataList = [];
-  let nextToken: string | undefined = undefined;
-  do {
-    const response: AWS.AuditManager.ListControlsResponse = await auditManager
-      .listControls({ controlType: 'Custom', nextToken })
-      .promise();
-    if (response.controlMetadataList) {
-      results.push(...response.controlMetadataList);
-    }
-    nextToken = response.nextToken;
-  } while (nextToken);
-  return results;
+    const results: AWS.AuditManager.ControlMetadataList = [];
+    let nextToken: string | undefined = undefined;
+    do {
+        const response: AWS.AuditManager.ListControlsResponse = await auditManager
+            .listControls({ controlType: 'Custom', nextToken })
+            .promise();
+        if (response.controlMetadataList) {
+            results.push(...response.controlMetadataList);
+        }
+        nextToken = response.nextToken;
+    } while (nextToken);
+    return results;
 }
 
-function buildControlRequest(
-  control: Control
-): AWS.AuditManager.CreateControlRequest {
-  return {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+function buildControlRequest(control: Control): AWS.AuditManager.CreateControlRequest {
+    return {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 
-    controlMappingSources: control.datasources!,
-    // [
-    //   {
-    //     sourceName: 'Manual Attestation',
-    //     sourceDescription: 'Manual attestation',
-    //     sourceSetUpOption: 'Procedural_Controls_Mapping',
-    //     sourceType: 'MANUAL',
-    //   },
-    //   {
-    //     sourceName: 'Manual Attestation',
-    //     sourceDescription: 'Config attestation',
-    //     sourceSetUpOption: 'System_Controls_Mapping',
-    //     sourceType: 'AWS_Config',
-    //   },
-    // ],
-    name: control.name,
-    actionPlanInstructions: control.actionPlanInstructions,
-    actionPlanTitle: control.actionPlanTitle,
-    description: control.description,
-    testingInformation: control.testingInformation,
-  };
+        controlMappingSources: control.datasources!,
+
+        name: control.name,
+        actionPlanInstructions: control.actionPlanInstructions,
+        actionPlanTitle: control.actionPlanTitle,
+        description: control.description,
+        testingInformation: control.testingInformation,
+    };
 }
 
 export async function processControlFile(dataFile: DataFile): Promise<void> {
-  const controlDefinition = dataFile.data as Control;
-  console.log(controlDefinition);
+    const controlDefinition = dataFile.data as Control;
+    console.log(controlDefinition);
 
-  // get the list of all custom controls
-  const controlList = await loadCustomControlList();
+    // get the list of all custom controls
+    const controlList = await loadCustomControlList();
 
-  // check whether or not the control already exists
-  const existingControl = controlList.find(
-    ({ name }) => name === controlDefinition.name
-  );
+    // check whether or not the control already exists
+    const existingControl = controlList.find(
+        ({ name }) => name === controlDefinition.name
+    );
 
-  if (!controlDefinition.datasources) {
-    controlDefinition.datasources = [
-      {
-        sourceName: 'Manual Attestation',
-        sourceDescription: 'Manual attestation',
-        sourceSetUpOption: 'Procedural_Controls_Mapping',
-        sourceType: 'MANUAL',
-      },
-    ];
-  }
-  // create or update the control
-  if (!existingControl) {
-    console.log('create new control');
-    const request = {
-      ...buildControlRequest(controlDefinition),
-      tags: controlDefinition.tags,
-    };
-    const response = await auditManager.createControl(request).promise();
-    console.log(response);
-  } else {
-    const controlId = existingControl.id || '';
-    const resourceArn = existingControl.arn || '';
-    console.log(`update existing control id = ${controlId}`);
-    const request: AWS.AuditManager.UpdateControlRequest = {
-      ...buildControlRequest(controlDefinition),
-      controlId,
-    };
-    const response = await auditManager.updateControl(request).promise();
-    console.log(response);
+    if (!controlDefinition.datasources) {
+        controlDefinition.datasources = [
+            {
+                sourceName: 'Manual Attestation',
+                sourceDescription: 'Manual attestation',
+                sourceSetUpOption: 'Procedural_Controls_Mapping',
+                sourceType: 'MANUAL',
+            },
+        ];
+    }
+    // create or update the control
+    if (!existingControl) {
+        console.log('create new control');
+        const request = {
+            ...buildControlRequest(controlDefinition),
+            tags: controlDefinition.tags,
+        };
+        const response = await auditManager.createControl(request).promise();
+        console.log(response);
+    } else {
+        const controlId = existingControl.id || '';
+        const resourceArn = existingControl.arn || '';
+        console.log(`update existing control id = ${controlId}`);
+        const request: AWS.AuditManager.UpdateControlRequest = {
+            ...buildControlRequest(controlDefinition),
+            controlId,
+        };
+        const response = await auditManager.updateControl(request).promise();
+        console.log(response);
 
-    //update tags
-    await updateTags(resourceArn, controlDefinition.tags);
-  }
+        //update tags
+        await updateTags(resourceArn, controlDefinition.tags);
+    }
 }
